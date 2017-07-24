62fca17be535ece59b6e6563ece6d3f9817b8944
tonyfast <tony.fast@gmail.com>
2017-07-23 20:28:43 -0400

    Add docstrings to source

--- importable.py
+++ importable.py
@@ -15,9 +15,11 @@ meta_paths = []
 
 
 try:
+    # Python 3 imports
     from importlib.util import spec_from_loader
     from importlib.machinery import SourceFileLoader as Base
 except:
+    # Python 2 imports
     from imputil import _FilesystemImporter as Base
 
 from nbconvert.exporters.base import export, get_exporter
@@ -32,10 +34,14 @@ PY2 = sys.version_info.major is 2
 
 class Importable(Base, object):
     def find_spec(self, name, paths, target=None):
+        # () -> ModuleSpec
+        """Python 3 finder uses the result from the Python 2 finder."""
         loader =  self.find_module(name, paths, target)
         return loader and spec_from_loader(name, loader)
     
     def find_module(self, name, paths, target=None):
+        # () -> Iportable
+        """Python 2 finder"""
         for ext in type(self.ext) is str and [self.ext] or self.ext:
             for path in paths or [curdir]:
                 path = extsep.join([sep.join([path, name.split('.')[-1]]), ext])
@@ -44,6 +50,7 @@ class Importable(Base, object):
         return None
     
     def get_code(self, path):
+        """Python 3 get_code from that returns a code object."""
         return compile(self.func(path), self.path, 'exec')
 
 
@@ -52,13 +59,14 @@ class Importable(Base, object):
 
 class Patched(Importable):
     def __init__(self, name=None, path=None):
-        self.name = name
-        self.path = path 
-    
+        self.name, self.path = name, path 
+        
     def load_module(self, name):
+        """Python 2 loader."""
         return self.import_top(name)
                                 
     def get_code(self, parent, name, fqname):
+        """Python 2 get_code function shim."""
         return 0, super(Patched, self).get_code(self.path), dict(__file__=self.path, __importer__=self)
 
 
@@ -72,36 +80,39 @@ exporter = get_exporter('python')()
 
 
 def add_finder(finder):
-    return sys.meta_path.append(finder(None, None)) or meta_paths.append(sys.meta_path[-1]) or finder
+    """Add a MetaFinder to the import manager."""
+    sys.meta_path.append(finder(None, None)) or meta_paths.append(sys.meta_path[-1])
+    return finder
 
 
 # In[7]:
 
 
 def finder(ext, bases=[], **kwargs):
-    """A decorator to create a new ext loader by defining the get_code method.
-    """
+    """A decorator to create a new ext loader by defining a custom func method."""            
     def importer(func):
         return add_finder(type(
-            func.__name__, 
-            (PY2 and Patched or Importable,), 
-            kwargs.update(func=func, ext=ext) or kwargs
-        ))
+            func.__name__, (PY2 and Patched or Importable,), kwargs.update(func=func, ext=ext) or kwargs))
     return importer
 
 
-# In[8]:
+# In[12]:
 
 
 def load_ipython_extension(ip=None):
+    """An Ipython extension to import notebooks.
+    
+    %reload_ext importable
+    """
     finder('ipynb')(Ipynb)
     sys.path_importer_cache.clear()
 
 
-# In[9]:
+# In[13]:
 
 
 def Ipynb(self, path):
+    """Custom loader from Ipynb files using the nbconvert exporter."""
     script = exporter.from_filename(self.path)[0]
     if PY2:
         script = '\n'.join(script.splitlines()[2:])
@@ -115,13 +126,14 @@ def Ipynb(self, path):
 
 def unload_ipython_extension(ip=None):
     for _ in sys.meta_path:
-        if _ in meta_paths: sys.meta_path.pop(sys.meta_path.index(_))
+        if _ in meta_paths: 
+            sys.meta_path.pop(sys.meta_path.index(_))
     sys.path_importer_cache.clear()
 
 
-# In[11]:
+# In[14]:
 
 
 if True and __name__ == '__main__': 
-    get_ipython().system('jupyter nbconvert --to script importable.ipynb')
+    get_ipython().system(u'jupyter nbconvert --to script importable.ipynb')
 
